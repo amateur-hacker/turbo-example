69de59f8c05c84c18142b548918f57c8c0e88d5e
amateur-hacker <amateurhacker2004@gmail.com>
2024-08-06 20:53:37 +0530

    Update api folder

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,28 +1,9 @@
-import { json, urlencoded } from "body-parser";
-import express, { type Express } from "express";
-import morgan from "morgan";
-import cors from "cors";
+import { createServer } from "./server";
 import { log } from "@repo/logger";
 
-const app = express();
-app
-  .disable("x-powered-by")
-  .use(morgan("dev"))
-  .use(urlencoded({ extended: true }))
-  .use(json())
-  .use(cors())
-  .get("/message/:name", (req, res) => {
-    return res.json({ message: `hello ${req.params.name}` });
-  })
-  .get("/status", (_, res) => {
-    return res.json({ ok: true });
-  });
-
 const port = process.env.PORT || 3001;
+const server = createServer();
 
-app.listen(port, () => {
+server.listen(port, () => {
   log(`api running on ${port}`);
 });
-
-
-export default app
